d83508e13e0a6b59b5e9f29e33f04560e140781f
Danilo Augusto <dasf_cm@terra.com.br>
2020-09-13 15:51:36 -0300

    feat: add some styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 /* eslint-disable no-console */
 const readline = require('readline-sync');
+const chalk = require('chalk');
+const figlet = require('figlet');
 const Category = require('./category/Category');
 const Food = require('./food/Food');
 const Guess = require('./guess/Guess');
 const { isOk } = require('./utils');
 
 const guessingGame = new Guess(Category, Food, readline);
+
+function welcome() {
+  function prepareWelcome() {
+    return figlet.textSync('Food Guessing Game', {
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 50,
+      whitespaceBreak: true,
+    });
+  }
+
+  console.log(chalk.yellow(prepareWelcome()));
+}
 
 function keepPlaying(answer = true) {
   if (!answer) {
@@ -15,6 +30,7 @@ function keepPlaying(answer = true) {
 }
 
 function startGame() {
+  welcome();
   while (true) {
     guessingGame.startGuessing();
     const answer = readline.question('Deseja continuar jogando? (s/n) ');
